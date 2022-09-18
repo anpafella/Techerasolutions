55cff3cbe988d9e88bd5382e618e3e2e3bf63a35
anpafella <98421859+anpafella@users.noreply.github.com>
2022-09-18 10:25:40 -0500

    Add files via upload

---
+++ src/main/java/com/techerasolutions/TecherasolutionsApplication.java
@@ -0,0 +1,13 @@
+package com.techerasolutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecherasolutionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TecherasolutionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techerasolutions/TecherasolutionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techerasolutions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TecherasolutionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
